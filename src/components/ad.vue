1ef4e0684054baaa101fbd557017b64f92c740f3
Wilson Hsu <wilsonhsu98@gmail.com>
2020-11-02 23:51:02 +0800

    try ad test2

--- src/components/ad.vue
+++ src/components/ad.vue
@@ -69,7 +69,7 @@ export default {
     return {
       show: true,
       callbackMethod: `vponCallBackMethod${new Date().getTime()}`,
-      configMode: 'test1', // test1, test2, production
+      configMode: 'test2', // test1, test2, production
       config: {
         test1: {
           adTest: 1,
@@ -84,7 +84,7 @@ export default {
         production: {
           adTest: 0,
           ...prodKey,
-          debug: false,
+          debug: true,
         },
       },
     };
