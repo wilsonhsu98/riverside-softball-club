2331d16006f700823f66b3d5912a9f332eb01069
Wilson Hsu <wilsonhsu98@gmail.com>
2020-12-05 07:30:21 +0800

    extend ad

--- src/components/ad.vue
+++ src/components/ad.vue
@@ -1,48 +1,108 @@
 <template>
-  <div v-if="show" ref="root" style="display: none;">
-    <vpon
-      v-if="mode === 'bottom_banner'"
-      vpon_ad_format="320x50_mb"
-      vpon_ad_adhesion="bottom"
-      :vpon_ad_test="config[configMode].adTest"
-      :vpon_ad_licensy_key="config[configMode].frequencyBannerKey"
-      :debug="config[configMode].debug"
-      :ad_request_callback="callbackMethod"
+  <div
+    v-if="show"
+    ref="root"
+    style="display: none;"
+    :style="{ height: height ? `${height}px` : '' }"
+  >
+    <div
+      :style="{
+        transform: scale ? `scale(${scale})` : '',
+        transformOrigin: scale ? 'top center' : '',
+      }"
     >
-    </vpon>
-    <vpon
-      v-if="['game_f', 'stats_pa_f', 'edit_team_f'].includes(mode)"
-      vpon_ad_format="mi"
-      :vpon_ad_test="config[configMode].adTest"
-      :vpon_ad_licensy_key="config[configMode].keys[mode]"
-      :debug="config[configMode].debug"
-      :ad_request_callback="callbackMethod"
-    >
-    </vpon>
-    <div class="only-mobile">
-      <vpon
-        v-if="['games', 'game', 'stats_item', 'user'].includes(mode)"
-        vpon_ad_format="320x50_mb"
-        :vpon_ad_test="config[configMode].adTest"
-        :vpon_ad_licensy_key="config[configMode].keys[mode]"
-        :debug="config[configMode].debug"
-        :ad_request_callback="callbackMethod"
-      >
-      </vpon>
-      <vpon
-        v-if="['stats_item300x250', 'user300x250'].includes(mode)"
-        vpon_ad_format="300x250_mb"
-        :vpon_ad_test="config[configMode].adTest"
-        :vpon_ad_licensy_key="config[configMode].keys[mode]"
-        :debug="config[configMode].debug"
-        :ad_request_callback="callbackMethod"
-      >
-      </vpon>
+      <template v-if="local">
+        <div
+          v-if="mode === 'bottom_banner'"
+          class="only-mobile fake-iframe"
+          vpon_ad_format="320x50_mb"
+          vpon_ad_adhesion="bottom"
+          style="position: fixed; right: 0; bottom: 0; left: 0; height: 50px; z-index: 9999;"
+          @click="show = false"
+        ></div>
+        <div
+          v-if="['game_f', 'stats_pa_f', 'edit_team_f'].includes(mode)"
+          class="only-mobile fake-iframe"
+          vpon_ad_format="mi"
+          style="position: fixed; top: 0; right: 0; bottom: 0; left: 0; z-index: 9999;"
+          @click="show = false"
+        ></div>
+        <div class="only-mobile">
+          <div
+            v-if="['games', 'game', 'stats_item', 'user'].includes(mode)"
+            class="fake-iframe"
+            vpon_ad_format="320x50_mb"
+            style="width: 320px; height: 50px;"
+          ></div>
+          <div
+            v-if="['stats_item300x250', 'user300x250'].includes(mode)"
+            class="fake-iframe"
+            vpon_ad_format="300x250_mb"
+            style="width: 300px; height: 250px;"
+          ></div>
+        </div>
+      </template>
+      <template v-else>
+        <vpon
+          v-if="mode === 'bottom_banner'"
+          vpon_ad_format="320x50_mb"
+          vpon_ad_adhesion="bottom"
+          :vpon_ad_test="config[configMode].adTest"
+          :vpon_ad_licensy_key="config[configMode].frequencyBannerKey"
+          :debug="config[configMode].debug"
+          :ad_request_callback="callbackMethod"
+        >
+        </vpon>
+        <vpon
+          v-if="['game_f', 'stats_pa_f', 'edit_team_f'].includes(mode)"
+          vpon_ad_format="mi"
+          :vpon_ad_test="config[configMode].adTest"
+          :vpon_ad_licensy_key="config[configMode].keys[mode]"
+          :debug="config[configMode].debug"
+          :ad_request_callback="callbackMethod"
+        >
+        </vpon>
+        <div class="only-mobile">
+          <vpon
+            v-if="['games', 'game', 'stats_item', 'user'].includes(mode)"
+            vpon_ad_format="320x50_mb"
+            :vpon_ad_test="config[configMode].adTest"
+            :vpon_ad_licensy_key="config[configMode].keys[mode]"
+            :debug="config[configMode].debug"
+            :ad_request_callback="callbackMethod"
+          >
+          </vpon>
+          <vpon
+            v-if="['stats_item300x250', 'user300x250'].includes(mode)"
+            vpon_ad_format="300x250_mb"
+            :vpon_ad_test="config[configMode].adTest"
+            :vpon_ad_licensy_key="config[configMode].keys[mode]"
+            :debug="config[configMode].debug"
+            :ad_request_callback="callbackMethod"
+          >
+          </vpon>
+        </div>
+      </template>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.fake-iframe {
+  margin: 0 auto;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: relative;
+  &:before {
+    content: attr(vpon_ad_format) '' attr(vpon_ad_adhesion);
+    color: #fff;
+    font-size: 20px;
+    display: inline-block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
 @media only screen and (min-width: 761px) {
   .only-mobile {
     display: none;
@@ -84,6 +144,7 @@ export default {
     };
 
     return {
+      local: false,
       show: true,
       callbackMethod: `vponCallBackMethod${new Date().getTime()}`,
       configMode: 'production', // test1, test2, production
@@ -104,6 +165,8 @@ export default {
           debug: true,
         },
       },
+      scale: undefined,
+      height: undefined,
     };
   },
   mounted() {
@@ -131,6 +194,20 @@ export default {
         }
         window[callbackMethod] = undefined;
       };
+      if (this.local) {
+        this.$refs.root.style.display = 'block';
+      }
+
+      if (document.body.clientWidth <= 414) {
+        if (['games', 'game', 'stats_item', 'user'].includes(this.mode)) {
+          this.scale = document.body.clientWidth / 320;
+          this.height = 50 * this.scale;
+        }
+        if (['stats_item300x250', 'user300x250'].includes(this.mode)) {
+          this.scale = document.body.clientWidth / 300;
+          this.height = 250 * this.scale;
+        }
+      }
     }
   },
 };
