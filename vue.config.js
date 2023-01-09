e0ff068a0ec7547f22d0c2fd5e518426f936bb29
Wilson Hsu <wilsonhsu98@gmail.com>
2023-01-09 16:53:55 +0800

    allow console

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,7 @@ module.exports = {
   runtimeCompiler: true,
   lintOnSave: process.env.NODE_ENV !== 'production',
   outputDir: './dist/client',
+  configureWebpack: config => {
+    config.optimization.minimizer[0].options.terserOptions.compress.drop_console = false;
+  },
 };
