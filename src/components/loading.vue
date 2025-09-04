24299e68e5f314442385227abc26718fb7cefa5b
Wilson Hsu <wilsonhsu98@gmail.com>
2025-09-04 15:36:11 +0800

    fix loading icon

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -1,19 +1,8 @@
 <template>
   <div class="loading-mask">
     <div class="logo">
-      <div class="img" :style="{ backgroundImage: `url(${defaultIcon})` }" />
+      <div class="img" ref="baseball" />
     </div>
-    <!-- <div v-else class="sk-cube-grid">
-      <div class="sk-cube sk-cube1"></div>
-      <div class="sk-cube sk-cube2"></div>
-      <div class="sk-cube sk-cube3"></div>
-      <div class="sk-cube sk-cube4"></div>
-      <div class="sk-cube sk-cube5"></div>
-      <div class="sk-cube sk-cube6"></div>
-      <div class="sk-cube sk-cube7"></div>
-      <div class="sk-cube sk-cube8"></div>
-      <div class="sk-cube sk-cube9"></div>
-    </div> -->
     <span v-if="text" class="text">{{ text }}</span>
   </div>
 </template>
@@ -33,45 +22,14 @@
 .logo {
   height: 100vh;
   height: calc(var(--vh, 1vh) * 100);
-  /* height: -webkit-fill-available; */
   text-align: center;
   .img {
-    width: 500px;
-    height: 500px;
-    // animation-name: spin;
-    // animation-duration: 2000ms;
-    // animation-iteration-count: infinite;
-    // animation-timing-function: linear;
+    width: 320px;
+    height: 320px;
     position: relative;
     left: 50%;
     top: 50%;
-    transform: translate(-50%, -50%) scale(calc(320 / 500));
-    background-repeat: no-repeat;
-    animation: play 2s steps(140) infinite;
-  }
-  &:after {
-    content: '';
-    height: 100%;
-    display: inline-block;
-    vertical-align: middle;
-  }
-}
-
-@keyframes play {
-  from {
-    background-position: 0 0;
-  }
-  to {
-    background-position: -70000px 0;
-  }
-}
-
-@keyframes spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
+    transform: translate(-50%, -50%);
   }
 }
 
@@ -84,99 +42,21 @@
   background-color: rgba(50, 122, 129, 0.2);
   z-index: 2;
 }
-
-.sk-cube-grid {
-  width: 40px;
-  height: 40px;
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  margin: auto;
-}
-
-.sk-cube-grid .sk-cube {
-  width: 33%;
-  height: 33%;
-  background-color: $loading_color;
-  float: left;
-  -webkit-animation: sk-cubeGridScaleDelay 1.3s infinite ease-in-out;
-  animation: sk-cubeGridScaleDelay 1.3s infinite ease-in-out;
-}
-.sk-cube-grid .sk-cube1 {
-  -webkit-animation-delay: 0.2s;
-  animation-delay: 0.2s;
-}
-.sk-cube-grid .sk-cube2 {
-  -webkit-animation-delay: 0.3s;
-  animation-delay: 0.3s;
-}
-.sk-cube-grid .sk-cube3 {
-  -webkit-animation-delay: 0.4s;
-  animation-delay: 0.4s;
-}
-.sk-cube-grid .sk-cube4 {
-  -webkit-animation-delay: 0.1s;
-  animation-delay: 0.1s;
-}
-.sk-cube-grid .sk-cube5 {
-  -webkit-animation-delay: 0.2s;
-  animation-delay: 0.2s;
-}
-.sk-cube-grid .sk-cube6 {
-  -webkit-animation-delay: 0.3s;
-  animation-delay: 0.3s;
-}
-.sk-cube-grid .sk-cube7 {
-  -webkit-animation-delay: 0s;
-  animation-delay: 0s;
-}
-.sk-cube-grid .sk-cube8 {
-  -webkit-animation-delay: 0.1s;
-  animation-delay: 0.1s;
-}
-.sk-cube-grid .sk-cube9 {
-  -webkit-animation-delay: 0.2s;
-  animation-delay: 0.2s;
-}
-
-@-webkit-keyframes sk-cubeGridScaleDelay {
-  0%,
-  70%,
-  100% {
-    -webkit-transform: scale3D(1, 1, 1);
-    transform: scale3D(1, 1, 1);
-  }
-  35% {
-    -webkit-transform: scale3D(0, 0, 1);
-    transform: scale3D(0, 0, 1);
-  }
-}
-
-@keyframes sk-cubeGridScaleDelay {
-  0%,
-  70%,
-  100% {
-    -webkit-transform: scale3D(1, 1, 1);
-    transform: scale3D(1, 1, 1);
-  }
-  35% {
-    -webkit-transform: scale3D(0, 0, 1);
-    transform: scale3D(0, 0, 1);
-  }
-}
 </style>
 
 <script>
-// import defaultIcon from '../images/Baseball.gif';
-import defaultIcon from '../images/Baseball_sprite.png';
+import Lottie from 'lottie-web';
+import baseballLottieData from '../images/baseballLottieData.json';
 export default {
   props: ['text', 'img'],
-  data() {
-    return {
-      defaultIcon,
-    };
+  mounted() {
+    Lottie.loadAnimation({
+      container: this.$refs.baseball,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: baseballLottieData,
+    });
   },
 };
 </script>
